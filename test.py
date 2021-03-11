a22c4b351dfe8ecaf914d96fa4e345b674acd194
chills-01 <carterhills@Carters-MBP.lan>
2021-03-11 17:52:23 +1100

    Removing unnecessary files [2].

--- test.py
+++
@@ -1,3 +0,0 @@
-import data_handling
-path_to_mv3d = '/Users/carterhills/SRP_2020:21/airway-labels/resources/M37-t2.a00.rec.scl3.Spatial-Graph.mv3d'
-print(data_handling.construct_dict_from_mv3d(path_to_mv3d))
